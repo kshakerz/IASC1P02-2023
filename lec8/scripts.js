78467efafa1f85874ef1ba7f8821c3230525288d
Kamran Shaikh <kamranshaikh8@yahoo.ca>
2023-11-05 11:48:34 -0500

    lab 8 and assignment 3 code

---
+++ lec8/scripts.js
@@ -0,0 +1,6 @@
+document.getElementById("one").style.color = "blue";
+document.getElementById("one").style.fontSize = "30px";
+document.getElementById("two").style.color = "green";
+document.getElementById("two").style.margin = "0px 100px";
+document.getElementById("three").style.color = "orange";
+document.getElementById("three").style.textTransform = "uppercase";
